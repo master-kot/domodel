dddf6dd1987dc356564e49130404a1f5c3de1fb1
master-kot <master-kot@mail.ru>
2020-08-06 01:21:52 +0300

    Приложение Домодел создано

---
+++ src/main/java/ru/geekbrains/domodel/DomodelApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.domodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomodelApplication.class, args);
+	}
+
+}
