4253a54efbecf4bbfd97dd54160d13d72f96ee14
master-kot <master-kot@mail.ru>
2020-08-07 00:41:07 +0300

    Исправлены недочеты в код стайл

--- src/main/java/ru/geekbrains/domodel/services/api/UserService.java
+++ src/main/java/ru/geekbrains/domodel/services/api/UserService.java
@@ -11,8 +11,20 @@ import ru.geekbrains.domodel.entities.UserRepresentation;
 @Service
 public interface UserService extends UserDetailsService {
 
+    /**
+     * Найти пользователя по логину
+     *
+     * @param login логин пользователя
+     * @return пользователь
+     */
     User findByLogin(String login);
 
+    /**
+     * Создать нового пользователя
+     *
+     * @param request отображение пользователя
+     * @return новый пользователь, сохраненный в репозитории
+     */
     User createNewUser(UserRepresentation request);
 
 }
---
+++ src/main/java/ru/geekbrains/domodel/services/core/MeterServiceImpl.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.domodel.services.core;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Реализация сервиса счетчиков
+ */
+@Service
+public class MeterServiceImpl {
+
+}
