26acdf8b5a7cd66b89c13b20339d3fe1462fd014
Artem Kropotov <sportarty@mail.ru>
2020-08-12 20:19:17 +0300

    Добавлен поиск по username в репе счетов

--- src/main/java/ru/geekbrains/domodel/repositories/BillRepository.java
+++ src/main/java/ru/geekbrains/domodel/repositories/BillRepository.java
@@ -3,11 +3,14 @@ package ru.geekbrains.domodel.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.domodel.entities.Bill;
+import ru.geekbrains.domodel.entities.User;
+
+import java.util.List;
 
 /**
  * Репозиторий счетов (платежных документов)
  */
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
-
+    List<Bill> findAllByUser(User user);
 }
