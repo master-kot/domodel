c60018409e1284de7788f9330c4320156d10a6c2
Nikolay Gavrilov <55625854+master-kot@users.noreply.github.com>
2020-08-14 22:36:58 +0300

    Репозиторий данных показаний счетчика изменен

--- src/main/java/ru/geekbrains/domodel/repositories/MeterDataRepository.java
+++ src/main/java/ru/geekbrains/domodel/repositories/MeterDataRepository.java
@@ -7,6 +7,15 @@ import ru.geekbrains.domodel.entities.MeterData;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Репозиторий данных показаний счетчика
+ */
 public interface MeterDataRepository extends JpaRepository<MeterData, Long> {
+    
+    /**
+     * Получить список показаний счетчика
+     */
     Optional<List<MeterData>> findByMeter(Meter meter);
+    
+    //TODO получить последнее по дате (самое свежее) показание счетчика
 }
