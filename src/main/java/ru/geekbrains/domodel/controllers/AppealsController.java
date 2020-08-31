7215f08d3348668812693bacfcce968a5fc88634
master-kot <master-kot@mail.ru>
2020-08-31 13:36:39 +0300

    Начат переход на REST. Создан временный контроллер, обеспечивающий отображение старого фронта. Все остальные контроллеры очищены от методов, преобразоваты к REST контроллерам и готовы к дальнейшей разработке

--- src/main/java/ru/geekbrains/domodel/controllers/AppealsController.java
+++ src/main/java/ru/geekbrains/domodel/controllers/AppealsController.java
@@ -1,33 +1,20 @@
 package ru.geekbrains.domodel.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.domodel.services.api.AppealsService;
-
-import java.security.Principal;
 
 /**
  * Контроллер обращений
  */
-@Controller
-@RequestMapping("/appeals")
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/appeals")
 @RequiredArgsConstructor
 public class AppealsController {
 
     // Сервис обращений
     private final AppealsService appealsService;
-
-    /**
-     * Перехват запроса главной страницы обращений
-     */
-    @GetMapping("")
-    public String getAppealsPage(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("username", principal.getName());
-        }
-        return "appeals/appeals_user";
-    }
 }
--- src/main/java/ru/geekbrains/domodel/controllers/NewsController.java
+++ src/main/java/ru/geekbrains/domodel/controllers/NewsController.java
@@ -1,58 +1,20 @@
 package ru.geekbrains.domodel.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.domodel.services.api.NewsService;
-
-import java.security.Principal;
 
 /**
  * Контроллер новостей
  */
-@Controller
-@RequestMapping("/news")
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
 public class NewsController {
 
     // Сервис новостей
     private final NewsService newsService;
-
-    /**
-     * Перехват запроса архива новостей
-     */
-    @GetMapping("")
-    public String getNewsArchivePage(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("username", principal.getName());
-        }
-        model.addAttribute("news", newsService.getAllNews());
-        return "news/news_archive";
-    }
-
-    /**
-     * Перехват запроса страницы редактирования новостей
-     */
-    @GetMapping("/edit")
-    public String getNewsEditPage(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("username", principal.getName());
-        }
-        model.addAttribute("news", newsService.getAllNews());
-        return "news/news_edit";
-    }
-
-    /**
-     * Перехват запроса страницы редактирования новостей
-     */
-    @GetMapping("/single")
-    public String getNewsSinglePage(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("username", principal.getName());
-        }
-        model.addAttribute("news", newsService.getAllNews());
-        return "news/news_single";
-    }
 }
