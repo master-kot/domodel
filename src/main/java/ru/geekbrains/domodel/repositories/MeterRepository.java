3fc07c5c969845004a21abe9a58e1b1e023a5345
Nikolay Gavrilov <55625854+master-kot@users.noreply.github.com>
2020-09-23 20:06:12 +0300

    Update MeterRepository.java

--- src/main/java/ru/geekbrains/domodel/repositories/MeterRepository.java
+++ src/main/java/ru/geekbrains/domodel/repositories/MeterRepository.java
@@ -27,18 +27,6 @@ public interface MeterRepository extends JpaRepository<Meter, Long> {
      */
 
     /**
-     * СОГЛАШЕНИЕ О НАИМЕНОВАНИИ МЕТОДОВ РЕПОЗИТОРИЕВ
-     * Optional<Object> findById(Long id) найти объект по параметру
-     * List<Object> findAll() найти все объекты
-     * List<Object> findAllByUser(User user) найти все объекты по параметру
-     * void delete(Object object) удалить конкретный объект
-     * Long deleteById(Long id) удалить объект по параметру
-     * void deleteAll(List<Object> objects) удалить список объектов
-     * Object save(Object object) сохранить объект
-     * List<Object> saveAll(List<Object> objects) сохранить список объектов
-     */
-
-    /**
      * Получить список счетчиков аккаунта
      */
     List<Meter> findByAccount(Account account);
